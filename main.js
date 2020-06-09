a750668bdaceb615a2458b71d5aa81599662f243
Sara <saracorsob@gmail.com>
2020-06-09 15:54:46 +0200

    Struttura click

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
+$(document).ready(
+  function() {
 
+    // Al click della freccia destra lo slider va avanti
+    $('.next').click(
+      function() {
+        alert('next');
+      }
+    );
+
+    // Al click della freccia sinistra lo slider torna indietro
+    $('.prev').click(
+      function() {
+        alert('prev');
+      }
+    );
+
+
+  }
+);
+
+
+// // FUNZIONI
+// function visualizzareImmagineSeguente() {
+//
+//
+// }
