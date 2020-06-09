f7296e10b71ccce6886b9009a665bbaca77b34e2
Sara <saracorsob@gmail.com>
2020-06-09 16:14:29 +0200

    Creazione prima funzione, al click del .next

--- main.js
+++ main.js
@@ -4,14 +4,14 @@ $(document).ready(
     // Al click della freccia destra lo slider va avanti
     $('.next').click(
       function() {
-        alert('next');
+        var sliderNext = visualizzareImmagineSeguente();
       }
     );
 
     // Al click della freccia sinistra lo slider torna indietro
     $('.prev').click(
       function() {
-        alert('prev');
+
       }
     );
 
@@ -19,9 +19,29 @@ $(document).ready(
   }
 );
 
+// var test = visualizzareImmagineSeguente();
 
-// // FUNZIONI
-// function visualizzareImmagineSeguente() {
-//
-//
-// }
+// FUNZIONI
+// Rimuovo la classe .active all'immagine e all'icona corrente,
+// La aggiungo alla immagine e icona successiva.
+// Argomento: nessuno
+// Return:
+function visualizzareImmagineSeguente() {
+  var immagineCorrente = $('.images img.active');
+  var pallinoCorrente = $('.nav i.active');
+
+  immagineCorrente.removeClass('active');
+  pallinoCorrente.removeClass('active');
+
+  // Quando l'elemento ha anche classe last, aggiungo
+  // la classe active all'elemento con classe first.
+  if ( immagineCorrente.hasClass('last') ) {
+    $('.images img.first').addClass('active');
+    $('.nav i.first').addClass('active');
+  }
+  else {
+    immagineCorrente.next('img').addClass('active');
+    pallinoCorrente.next('i').addClass('active');
+  }
+
+}
